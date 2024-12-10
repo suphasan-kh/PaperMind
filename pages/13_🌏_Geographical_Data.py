d84991c346940b0766a65bbbc9938e94ac14d2bb
Suphasan Khempong <148676084+suphasan-kh@users.noreply.github.com>
2024-12-10 16:39:34 +0700

    Update 13_🌏_Geographical_Data.py

--- pages/13_🌏_Geographical_Data.py
+++ pages/13_🌏_Geographical_Data.py
@@ -2,6 +2,7 @@ import streamlit as st
 from pymongo.mongo_client import MongoClient
 from geopy.geocoders import Nominatim
 import pandas as pd
+import pydeck as pdk
 
 gl = Nominatim(user_agent='ds')
 
@@ -48,19 +49,44 @@ if author.strip().count(' ') == 1:
 
         affiliation = cd['affiliation']
         if type(affiliation).__name__ == 'dict':
-            location = gl.geocode(affiliation['affiliation-city'] + ' ' + affiliation['affiliation-country'])
+            location = gl.geocode(affiliation['affilname'] + ' ' + affiliation['affiliation-city'] + ' ' + affiliation['affiliation-country'])
+            if location == None:
+                location = gl.geocode(affiliation['affiliation-city'] + ' ' + affiliation['affiliation-country'])
             coord.append([location.latitude,location.longitude,affiliation['affilname']])
         else:
             for af in affiliation:
                 if af['@id'] in city_id:
-                    location = gl.geocode(af['affiliation-city'] + ' ' + af['affiliation-country'])
+                    location = gl.geocode(af['affilname'] + ' ' + af['affiliation-city'] + ' ' + af['affiliation-country'])
+                    if location == None:
+                        location = gl.geocode(af['affiliation-city'] + ' ' + af['affiliation-country'])
                     coord.append([location.latitude,location.longitude,af['affilname']])
-
+                    # coord.append([location.latitude,location.longitude])
+        
+        
         coord = pd.DataFrame(coord)
         coord = coord.rename(columns = {0:'latitude',1:'longitude',2:'info'})
+
+        layer = pdk.Layer(
+            "ScatterplotLayer",
+            coord,
+            pickable=True,
+            opacity=0.8,
+            stroked=True,
+            filled=True,
+            radius_scale=6,
+            radius_min_pixels=5,
+            radius_max_pixels=100,
+            line_width_min_pixels=1,
+            get_position=['longitude','latitude'],
+            get_fill_color=[255, 10, 10],
+            get_line_color=[255, 255, 255],
+        )
+
+        r = pdk.Deck(layers=[layer], tooltip={"text": "{info}"})
         
         st.write("# Affiliation Map")
-        st.map(coord)
+        st.pydeck_chart(r)
+        # st.map(coord, size=500)
         st.write("# Affiliation Locations")
         st.write(coord)
         st.sidebar.write('Successful')
